547bc8fea5ec2f004b41c6a9bbf30c8c8cd9f29f
Evan Henry <evan@bisontechllc.com>
2016-12-16 08:59:07 -0500

    Update woocommerce-genesis.php

--- woocommerce-genesis.php
+++ woocommerce-genesis.php
@@ -39,7 +39,7 @@ function display_price_in_variation_option_name( $term ) {
 }
 
 //* Force all products to one page - eliminating pagination 
-add_filter( 'loop_shop_per_page', create_function( '$cols', 'return -1;' ), 20 );
+add_filter( 'prefix_shop_per_page', create_function( '$cols', 'return -1;' ), 20 );
 
 //* Remove tabs from product details page
 add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
@@ -53,5 +53,54 @@ function woo_remove_product_tabs( $tabs ) {
 	return $tabs;
 
 }
+
+// Hides the product's weight and dimension in teh single product page.
+add_filter( 'wc_product_enable_dimensions_display', '__return_false' );
+
+//* Replace the image filename/path with custom image
+add_action( 'init', 'prefix_fix_thumbnail' );
+
+function prefix_fix_thumbnail() {
+	add_filter( 'woocommerce_placeholder_img_src', 'custom_woocommerce_placeholder_img_src' );
+
+	function custom_woocommerce_placeholder_img_src( $src ) {
+		$upload_dir = wp_upload_dir();
+		$uploads    = untrailingslashit( $upload_dir['baseurl'] );
+		// add file path to new placeholder image
+		$src        = $uploads . '/new-coming-soon.jpg';
+
+		return $src;
+	}
+}
+
+// Add necessary class to WooCommerce pages for FacetWP
+add_filter( 'genesis_attr_content', 'custom_attributes_content' );
+function custom_attributes_content( $attributes ) {
+
+	if ( is_shop() || is_product() || is_product_category() || is_product_tag() ) {
+		$attributes['class'] .= ' facetwp-template';
+	}
+
+	return $attributes;
+
+
+}
+
+// Remove Reset button from Price Facet (slider type)
+function prefix_facetwp_facet_html( $output, $params ) {
+	if ( 'price' == $params['facet']['name'] ) {
+		$output = '';
+		$value  = $params['selected_values'];
+		$output .= '<div class="facetwp-slider-wrap">';
+		$output .= '<div class="facetwp-slider"></div>';
+		$output .= '</div>';
+		$output .= '<span class="facetwp-slider-label"></span>';
+		$output .= '<div><input type="button" class="facetwp-slider-reset" value="' . __( 'Reset', 'fwp' ) . '" /></div>';
+	}
+
+	return $output;
+}
+
+add_filter( 'facetwp_facet_html', 'prefix_facetwp_facet_html', 10, 2 );
 
 ?>
